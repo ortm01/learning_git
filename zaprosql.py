70d042b94f33e28c947f8794c331d86fcbf1a320
artem <bilinairen@gmail.com>
2026-09-25 16:24:34 +0700

    Probels dels

--- zaprosql.py
+++ zaprosql.py
@@ -1,11 +1,6 @@
 import pandas as pd
 from sqlalchemy import create_engine
-
-# 1. Данные для подключения
-# Шаблон: postgresql://логин:пароль@localhost:5432/имя_базы
-
 engine = create_engine('postgresql://artem:1234@localhost:5432/mydb')
-# 2. Наш прошлый SQL-запрос с оконной функцией
 query = """
 SELECT 
     name,
@@ -19,11 +14,7 @@ SELECT
 FROM stafs
 WHERE dept_id IS NOT NULL;
 """
-
-# 3. Чтение SQL-запроса напрямую в Pandas DataFrame
 df = pd.read_sql(query, engine)
-
-# Вывод результата в консоль
 print(df)
 print('='*30)
 print(df[df['salary'] > 3000])
